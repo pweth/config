662d3a31b992283be7446459b7b979218fa2ebd3
pweth <22416843+pweth@users.noreply.github.com>
2023-08-20 23:28:29 +0100

    Gnome tweaks

--- configuration.nix
+++ configuration.nix
@@ -116,11 +116,20 @@ in
         "org/gnome/desktop/interface" = {
           color-scheme = "prefer-dark";
           enable-hot-corners = false;
+          show-battery-percentage = true;
           text-scaling-factor = 1.3;
+        };
+        "org/gnome/desktop/notifications" = {
+          show-in-lock-screen = false;
+        };
+        "org/gnome/mutter" = {
+          dynamic-workspaces = true;
+        };
+        "org/gnome/settings-daemon/plugins/color" = {
+          night-light-enabled = true;
         };
       };
     };
-
   };
 
   # Allow unfree packages
