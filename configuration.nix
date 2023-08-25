74906d2e2d495ba1c0b55d0c406a87080b482a9b
pweth <22416843+pweth@users.noreply.github.com>
2023-08-25 13:29:52 +0100

    Firefox hardening; difftastic

--- configuration.nix
+++ configuration.nix
@@ -102,12 +102,9 @@ in
 
   # User account
   users.users.pweth = {
-    createHome = true;
     description = "Peter";
     extraGroups = [ "networkmanager" "wheel" ];
-    home = "/home/pweth";
     isNormalUser = true;
-    uid = 1000;
   };
 
   # Home manager
@@ -130,8 +127,68 @@ in
           };
           search.force = true;
           settings = {
+            # Change Enhanced Tracking Protection to strict
+            "browser.contentblocking.category" = "strict"; 
+            # Disable new tab page
+            "browser.newtab.url" = "about:blank";
+            "browser.newtabpage.activity-stream.enabled" = false;
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
             "browser.newtabpage.enabled" = false;
-            "browser.startup.homepage" = "about:blank";
+            "browser.ping-centre.telemetry" = false;
+            # Set search region to UK
+            "browser.search.region" = "GB";
+            "browser.search.isUS" = false;
+            # Set home page
+            "browser.startup.homepage" = "https://start.duckduckgo.com";
+            # Clean up the URL bar
+            "browser.urlbar.shortcuts.bookmarks" = false;
+            "browser.urlbar.shortcuts.history" = false;
+            "browser.urlbar.shortcuts.tabs" = false;
+            "browser.urlbar.showSearchSuggestionsFirst" = false;
+            "browser.urlbar.speculativeConnect.enabled" = false;
+            "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+            "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+            "browser.urlbar.suggest.searches" = false;
+            # Disable health reports
+            "datareporting.healthreport.service.enabled" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "datareporting.policy.dataSubmissionEnabled" = false;
+            # Dev tools theme
+            "devtools.theme" = "dark";
+            # Disable battery API
+            "dom.battery.enabled" = false;
+            # Disable gamepad API
+            "dom.gamepad.enabled" = false;
+            # Disable experiments
+            "experimens.activeExperiment" = false;
+            "experiments.enabled" = false;
+            "experiments.manifest.uri" = "";
+            "experiments.supported" = false;
+            "network.allow-experiments" = false;
+            # Disable pocket
+            "extensions.pocket.enabled" = false;
+            # Privacy settings
+            "privacy.donottrackheader.enabled" = true;
+            "privacy.donottrackheader.value" = 1;
+            "privacy.purge_trackers.enable" = true;
+            # Disable the built-in password manager
+            "signon.rememberSignons" = false;
+            # Disable telemetry
+            "toolkit.coverage.endpoint.base" = "";
+            "toolkit.coverage.opt-out" = true;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.eabled" = false;
+            "toolkit.telemetry.coverage.opt-out" = true;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.hybridContent.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "toolkit.telemetry.server" = "data:,";
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
           };
         };
       };
@@ -142,6 +199,9 @@ in
       enable = true;
       userName = "pweth";
       userEmail = "22416843+pweth@users.noreply.github.com";
+      # Use the difftastic syntax highlighter
+      difftastic.enable = true;
+      difftastic.color = "always";
     };
 
     # Gnome dconf settings
