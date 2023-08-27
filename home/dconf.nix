3ad1c59de213fc3956d671183f51cbc9a61246d1
pweth <22416843+pweth@users.noreply.github.com>
2023-08-27 18:42:44 +0100

    GNOME tweaks dconf updates

--- home/dconf.nix
+++ home/dconf.nix
@@ -8,14 +8,28 @@
         picture-uri = "file:///home/pweth/dotfiles/assets/background.jpg";
         picture-uri-dark = "file:///home/pweth/dotfiles/assets/background.jpg";
       };
+      "org/gnome/desktop/calendar" = {
+        show-weekdate = true;
+      };
       "org/gnome/desktop/interface" = {
+        clock-show-weekday = true;
         color-scheme = "prefer-dark";
         enable-hot-corners = false;
+        gtk-enable-primary-paste = false;
         show-battery-percentage = true;
         text-scaling-factor = 1.3;
       };
+      "org/gnome/desktop/peripherals/touchpad" = {
+        click-method = "areas";
+      };
       "org/gnome/desktop/notifications" = {
         show-in-lock-screen = false;
+      };
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file:///home/pweth/dotfiles/assets/background.jpg";
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,close";
       };
       "org/gnome/mutter" = {
         dynamic-workspaces = true;
