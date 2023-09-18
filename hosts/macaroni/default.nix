b2441a0913d7ab457fbb62b7114c209124a97a52
pweth <22416843+pweth@users.noreply.github.com>
2023-09-18 18:47:41 +0100

    Disable root login on VM

--- hosts/macaroni/default.nix
+++ hosts/macaroni/default.nix
@@ -19,11 +19,6 @@
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
-    settings.PermitRootLogin = "yes";
+    settings.PermitRootLogin = "no";
   };
-
-  # TMP
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2LcPpOlnOwQ67Xp6uJnuOmDj0W06Bzyr73l6xkZgtg me@pweth.com"
-  ];
 }
