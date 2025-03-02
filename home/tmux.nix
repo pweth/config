a1d4e605e03efc7e86a17bc9744a5b5a5b0ca0ad
pweth <22416843+pweth@users.noreply.github.com>
2025-03-02 15:55:01 +0000

    Start tmux numbering at 1

--- home/tmux.nix
+++ home/tmux.nix
@@ -8,6 +8,7 @@
     baseIndex = 1;
     mouse = true;
     extraConfig = ''
+      set -g base-index 1
       set -g mouse on
       set -g pane-active-border-style 'bg=default fg=colour239'
       set -g pane-border-style 'bg=default fg=colour236'
@@ -18,6 +19,7 @@
       set -g status-position top
       set -g status-right '#[fg=colour231, bg=colour233] #{session_name} → #(hostname) '
       set -g status-style 'bg=default fg=colour137'
+      setw -g pane-base-index 1
       setw -g window-status-current-format ' #I #W#F '
       setw -g window-status-current-style 'fg=colour231 bg=colour233 bold'
       setw -g window-status-format ' #I #W#F '
