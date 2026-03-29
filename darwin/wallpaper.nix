c02e80732d946bec306aff22f1b0e390d9a0a560
pweth <22416843+pweth@users.noreply.github.com>
2026-03-29 19:13:19 +0100

    Fix daily wallpaper daemon

--- darwin/wallpaper.nix
+++ darwin/wallpaper.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 let 
   script = pkgs.writeShellScriptBin "update-wallpaper" ''
-    ${pkgs.curl}/bin/curl -L "https://pweth.com/noindex/img/background.jpg" -o "$HOME/Wallpaper.jpg"
+    ${pkgs.curl}/bin/curl -L "https://pweth.com/noindex/img/background.jpg" -o "$HOME/Wallpaper.tmp.jpg" \
+      && mv "$HOME/Wallpaper.tmp.jpg" "$HOME/Wallpaper.jpg"
     /usr/bin/osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$HOME/Wallpaper.jpg\""
+    /usr/bin/killall WallpaperAgent
   '';
 in 
 {
@@ -10,7 +12,7 @@ in
   environment.systemPackages = [ script ];
 
   # Launchd service
-  launchd.user.agents.bing-wallpaper = {
+  launchd.user.agents.update-wallpaper = {
     serviceConfig = {
       ProgramArguments = [ "${script}/bin/update-wallpaper" ];
       RunAtLoad = true;
